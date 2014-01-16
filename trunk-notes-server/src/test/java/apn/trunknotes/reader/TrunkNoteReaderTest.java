acd81c686fe64dec30ec97ffedab4e7e7469337f
antmyth <antonio.p.nascimento@gmail.com>
2014-01-16 21:28:58 +0000

    trunk-notes-server-1 Visualise single trunk notes pages by name, not parsing functions : test now uses try with resources

--- trunk-notes-server/src/test/java/apn/trunknotes/reader/TrunkNoteReaderTest.java
+++ trunk-notes-server/src/test/java/apn/trunknotes/reader/TrunkNoteReaderTest.java
@@ -5,12 +5,13 @@ import apn.trunknotes.types.TrunkNote;
 import org.junit.Test;
 
 import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 
 import static basecamp.datafixtures.PrimitiveDataFixtures.someString;
+import static java.nio.file.Files.newBufferedWriter;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
@@ -21,15 +22,18 @@ public class TrunkNoteReaderTest {
         Path tempFile = Files.createTempFile(someString(), "markdown");
         Title title = new Title(someString());
 
-        File noteFile = tempFile.toFile();
-        assertThat(true,is(noteFile.exists()));
-        BufferedWriter writer = new BufferedWriter(new FileWriter(noteFile));
-        writer.write(String.format("Title: %s\n",title.asString()));
-        writer.close();
+        assertThat(true, is(tempFile.toFile().exists()));
+
+        try (BufferedWriter writer =
+                     newBufferedWriter(tempFile, StandardCharsets.UTF_8,
+                             StandardOpenOption.WRITE)) {
+            writer.write(String.format("Title: %s\n", title.asString()));
+        }
+
 
         TrunkNoteReader reader = new TrunkNoteReader();
         TrunkNote expected = new TrunkNote(title);
 
-        assertThat(expected, is(reader.load(noteFile)));
+        assertThat(expected, is(reader.load(tempFile.toFile())));
     }
 }
